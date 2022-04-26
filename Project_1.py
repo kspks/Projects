ff563fd6585fe309ee19e9702288c620a1905929
Kseniia <90192541+kspks@users.noreply.github.com>
2022-04-26 11:40:22 +0200

    Update Project_1.py
    Medical Insurance Estimates vs. Costs Project.

--- Project_1.py
+++ Project_1.py
@@ -1,5 +1,12 @@
+# Project based on using Loops for define Medical Insurance Estimates vs. Costs Project.
+# I am interested in analyzing medical insurance cost data efficiently without writing repetitive code.
+# In this project, I was use my knowledge of Python loops to iterate through and analyze medical insurance cost data.
+
+# The names of fiveteen individuals.
 names = ["Judith", "Abel", "Tyson", "Martha", "Beverley", "David", "Anabel", "Liam", "Noah", "Emma", "Olivia", "  Oliver", "Ava", " Charlotte", " Elijah"]
+# Estimated medical insurance costs for the individuals.
 estimated_insurance_costs = [4321.0, 6440.0, 5599.0, 2193.0, 5773.0, 7694.0, 3797.0, 3662.0, 2402.0, 4535.0, 3154.0, 3122.0, 4801.0, 4758.0, 4488.0]
+# The actual insurance costs paid by the individuals.
 actual_insurance_costs = [4753.1, 7084.0, 6158.9, 2412.3, 6350.3, 8463.4, 4176.7, 4028.2, 2642.2, 4988.5, 3469.4, 3434.2, 5281.1, 5233.8, 4936.8]
 
 # Add your code here
